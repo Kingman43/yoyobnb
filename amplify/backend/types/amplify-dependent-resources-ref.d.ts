2abad41f7b842001d7d2ffd36e3a84fbb5178215
Kingman43 <44009057+Kingman43@users.noreply.github.com>
2023-01-10 17:21:36 -0500

    Added Amplify Auth
    Auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "yoyobnb106230b8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
